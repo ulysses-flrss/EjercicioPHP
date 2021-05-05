53ec9a83e299446ee181316e81ef7050eabd3f18
ulysses-flrss <66712163+ulysses-flrss@users.noreply.github.com>
2021-05-05 17:04:09 -0600

    Cambios

--- index.php
+++ index.php
@@ -1,40 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio PHP - Equipo 6</title>
-  </head>
+<html>
+<head>
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" type="text/css" href="Perfil.css">
+</head>
+
+<body>
+    <center>
+    <br><br><br>
+    <form action="Empleado/empleado.php" method = "get">
+      <label for="Correo">Ingrese Su codigo:</label>
+      <br><br>
+      <input type="text" name="Usuario" id="usuario" placeholder="Ejemplo: B1" required>
+      <br><br>
+      <label for="Contraseña">Ingrese Su Contraseña:</label>
+      <br><br>
+      <input type="password" name="Contraseña" id="Contraseña" placeholder="" required>
+      <input type="submit" value="Iniciar Sesion" class="enviar">
+    </form>
+  </center> 
+
+  <?php 
   
-  <body>
-    <?php 
-      $conectar = @mysqli_connect('localhost', 'root', '', 'registrostmh');
-        if (!$conectar) {
-          echo "ERROR: No se puede conectar al SERVIDOR";
-        } else {
-            $data = @mysqli_select_db($conectar,'registrostmh');
-            if (!$data) {
-                echo "ERROR: No se puede conectar a la BASE DE DATOS";
-            }
-        }
-      
-      $Usuario = $_GET['Usuario'];
-      $Contraseña = $_GET['Contraseña'];
+    $_SESSION['Usuario'] = array($);
 
-      $query = "SELECT * FROM datos WHERE  Usuario LIKE '".$Usuario."' AND Contraseña LIKE  '".$Contraseña."' ";  
-      $q = mysqli_query($conectar, $query);
+  ?>
 
-      $fila = mysqli_num_rows($q);
-
-      if ($fila > 0) {
-          header ('../index.html')
-      } else {
-          echo "<link href = '../estilosregister.css' type = 'text/css' rel = 'stylesheet'> <p class = 'stylePassword'>Correo o Contraseña Incorrectos</p> <a href = ../Español/login.html>Volver a Intentar</a>";
-      }   mysqli_free_result($q);
-    ?>
-  </body>
-<<<<<<< Updated upstream
+</body>
 </html>
-=======
-</html>
->>>>>>> Stashed changes
