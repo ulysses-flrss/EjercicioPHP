f6953958f41fe5dc2d9de4172e971f102d9a3a6d
Guillermo-31 <guillermogr211@gmail.com>
2021-05-05 16:54:14 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,36 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-    <title>Iniciar Sesión</title>
-    <link rel="stylesheet" type="text/css" href="Perfil.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Iniciar Sesión</title>
+  <link rel="stylesheet" type="text/css" href="Perfil.css">
 </head>
-<<<<<<< HEAD
-<body>
-    <div class="loginbox">
-    <form>
-    <label for="username">Username: </label>
-    <input type="text" name="usuario" placeholder="Username">
-    <label for="password">Contraseña: </label>
-    <input type="password" name="pass" placeholder="Enter your password">
-    <input type="submit" name="passaway" placeholder="login">
-    </form>
-    </div>
-=======
 
-<body>
+  <body>
     <center>
-    <br><br><br>
-    <form action="../PHP/login.php">
-      <label for="Correo">Ingrese Su codigo:</label>
-      <br><br>
-      <input type="text" name="Usuario" id="usuario" placeholder="Ejemplo: B1" required>
-      <br><br>
-      <label for="Contraseña">Ingrese Su Contraseña:</label>
-      <br><br>
-      <input type="password" name="Contraseña" id="Contraseña" placeholder="" required>
-      <input type="submit" value="Iniciar Sesion" class="enviar">
-    </form>
-  </center>
->>>>>>> e902f6688b3b962439e9b08b50ca236aa06e5fa5
-</body>
+      <br><br><br>
+      <form action="../PHP/login.php">
+        <label for="Correo">Ingrese Su Código de Empleado: </label>
+        <br><br>
+        <input type="text" name="Usuario" id="usuario" placeholder="Ejemplo: B1" required>
+        <br><br>
+        <label for="Contraseña">Ingrese Su Contraseña: </label>
+        <br><br>
+        <input type="password" name="Contraseña" id="Contraseña" placeholder="" required>
+        <input type="submit" value="Iniciar Sesion" class="enviar">
+      </form>
+    </center>
+  </body>
 </html>
