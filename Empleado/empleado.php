6b2c218e321698b2c9bc1010e8d22b218cd14bb5
ulysses-flrss <66712163+ulysses-flrss@users.noreply.github.com>
2021-05-05 15:49:03 -0600

    Enter

---
+++ Empleado/empleado.php
@@ -0,0 +1,16 @@
+<?php 
+
+    session_start();
+
+    $_SESSION['empleado'];
+
+    $codigoEmpleado;
+    $nombreCompleto;
+    $cargoEmpleado;
+    $sueldoBase;
+    $descuentoISSS;
+    $descuentoAFP;
+    $descuentoRenta;
+    $sueldoLiquido;
+
+?>
