2186d0a76a5916d5a93b855f4ec820eda4061787
ulysses-flrss <66712163+ulysses-flrss@users.noreply.github.com>
2021-05-05 22:28:22 -0600

    .

--- validacion.php
+++ validacion.php
@@ -1,6 +1,6 @@
 <?php 
 
-    //ID's, Nombres, Cargos, Sueldo
+    //ID's
 
     $_SESSION['id'] = array("001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013", "014", "015");
 
@@ -22,4 +22,12 @@
             }
     }
 
+
+    //Nombres, Cargos, Sueldo
+
+    $_SESSION['names'] = array("Acosta Torres, Susana Alejandra", "Alvarez Flores, Karla Marcela", "Castillo, Daniel Humberto", "Dybala Dos Santos, Paulo Bruno", "Gonzalez Gómez, Luis Alonso", "Granadino Rivas, Kevin Alejandro", "Hernadez Henriquez, Luis Felipe", "Mejia Escobar, Michelle Abigail", "Mendoza Portillo, José Miguel", "Moura Juarez, Lucas Alberto", "Panameño Molina, Daniel Alberto", "Pérez Aguila, Juan Antonio", "Pineda Pineda, Judith Carolina", "Quintero Paredes, Armando David", "Zelaya Monterosa, José Alexis";
+);
+    $_SESSION['cargos'] = array("Jefe", "Gerente", "Supervisor", "Empleado");   
+
+    $_SESSION['sueldo'] = array(400, 1350, 900, 1150, 450, 1200, 500, 700, 1000, 600, 430, 500, 950, 480, 800);
 ?>
