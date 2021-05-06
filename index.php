ed205ddf1ddde8a6356c0aa9c229d25f5dfa167a
ulysses-flrss <66712163+ulysses-flrss@users.noreply.github.com>
2021-05-05 21:40:45 -0600

    .

--- index.php
+++ index.php
@@ -17,9 +17,7 @@
       <br><br>
       <input type="password" name="password" id="password" placeholder="" required>
       <input type="submit" value="Iniciar Sesion" class="enviar">
-    </form>
-    <a href="login Contador.php">Inicia Sesión como Contador</a> <!-- si se te ocurre algo mejor, lo pones -->
-    
+    </form>    
   </center> 
 
  
